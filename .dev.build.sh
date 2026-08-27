d34a52f6884de82b545faec94dee5aea368fee34
TheOninesixY <theoninesixy@outlook.com>
2026-08-27 22:22:31 +0800

    a

--- .dev.build.sh
+++ .dev.build.sh
@@ -1,2 +1,2 @@
-flatpak-builder --repo=FlatHMCL --force-clean build-dir io.github.theoninesixy.FlatHMCL.yml
+flatpak-builder --gpg-sign=7521D8DCF247D74E --repo=FlatHMCL --force-clean build-dir io.github.theoninesixy.FlatHMCL.yml
 flatpak build-bundle FlatHMCL FlatHMCL.flatpak io.github.theoninesixy.FlatHMCL
